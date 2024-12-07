da69af5637d2333ecb73b8d169a6e786d07e4844
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-12-07 19:48:06 +0530

    Feat:Developed the Reviews Component Page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,6 +4,7 @@ import Rules from "./Components/User/Rules";
 import Timer from "./Components/User/Timer";
 import LoadingPage from "./Components/User/LoadingPage";
 import ScorePage from "./Components/User/ScorePage";
+import Reviews from "./Components/User/Reviews";
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -30,6 +31,10 @@ function App() {
     {
       path: "/score",
       element: <ScorePage />,
+    },
+    {
+      path: "/reviews",
+      element: <Reviews />,
     },
   ]);
 
