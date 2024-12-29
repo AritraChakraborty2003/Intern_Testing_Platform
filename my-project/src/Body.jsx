fd74811e0571d923cc2088024ace06f5a979a5bc
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-12-29 14:17:55 +0530

    Update in the logic

---
+++ my-project/src/Body.jsx
@@ -0,0 +1,34 @@
+import App from "./App";
+import { useState } from "react";
+import { UserContext } from "./UserContext/UserContext";
+
+const Body = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [domain, setDomain] = useState("");
+  const [ansList, setAnsList] = useState([]);
+  const [correctAnsList, setCorrectAnsList] = useState([]);
+
+  return (
+    <>
+      <UserContext.Provider
+        value={{
+          name: name,
+          setName: setName,
+          email: email,
+          setEmail: setEmail,
+          domain: domain,
+          setDomain: setDomain,
+          ansList: ansList,
+          setAnsList: setAnsList,
+          correctAnsList: correctAnsList,
+          setCorrectAnsList: setCorrectAnsList,
+        }}
+      >
+        <App />
+      </UserContext.Provider>
+    </>
+  );
+};
+
+export default Body;
